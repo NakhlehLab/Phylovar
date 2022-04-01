c15a470a5c687c27f17ab6efc7227841e7c8c4b4
Mohammadamin Edrisi <31323695+mae6@users.noreply.github.com>
2022-04-01 02:29:57 -0500

    Add files via upload

--- indexing_scripts/local_index_recovery.py
+++ indexing_scripts/local_index_recovery.py
@@ -1,50 +1,31 @@
+import argparse
+
 if __name__=="__main__":
-	mpileup_path = "/shared/mae6/snv_calling_data/test/tnbc_global_idx.mpileup"
-	index_path = "/shared/mae6/snv_calling_data/test/index.csv"
-	out_path = "/shared/mae6/snv_calling_data/test/tnbc_local_idx.mpileup"
-	genotype_path = "/shared/mae6/snv_calling_data/test/genotype_matrix.csv"
-	
-	genotypes = []
-	line_lens = set()
-	with open(genotype_path, "r") as genotypef:
-		for line in genotypef:
-			if "pos" in line:
-				pass
-			else:
-				s = line.strip().split(",")
-				line_lens.add(len(s))
-				if len(genotypes)==0 or (s[0]!=genotypes[-1]):
-					genotypes.append(s[0])
-	genotypef.close()
-	print(len(genotypes))
-	if len(genotypes)==len(set(genotypes)):
-		print("genotype matrix file does not have duplicates")
-	else:
-		print("genotype matrix file does have duplicates!!!")
-	print("lengths of the lines in the genotype matrix file")
-	print(line_lens)
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-mpileup", "--mpileup", required=True,
+		help="path to the original mpileup")
+	ap.add_argument("-sciphi", "--sciphi", required=True,
+		help="path to the output of SCIPhi after filtering non-informative sites named genotype_matrix.csv")
+	ap.add_argument("-out", "--out", required=True,
+		help="path to the output mpileup")
+	args = ap.parse_args()
+	mpileup_path = args.mpileup
+	out_path = args.out
+	genotype_path = args.sciphi
 
-	idx_dict = {}
-	with open(index_path, "r") as idxf:
-		for line in idxf:
-			s = line.strip().split(",")
-			idx_dict[s[0]] = s[2]
-	idxf.close()
-	print(len(idx_dict))
-
-	line_counter = 0
 	outf = open(out_path, "w")
-	with open(mpileup_path, "r") as infile:
-		for line in infile:
-			if len(genotypes)==0:
-				break
-			elif str(line_counter)==genotypes[0]:
-				s = line.split("\t")
-				s[1] = idx_dict[str(line_counter)]
-				outf.write("\t".join(s))
-				genotypes.pop(0)
+	genotype_f = open(genotype_path, "r")
+	# the first line is the headers
+	genotype_f.readline()
+	original_mpileup = open(mpileup_path, "r")
+	line_counter = 0
+	original_line = original_mpileup.readline()
+
+	for line in genotype_f:
+		global_index = int(line.strip().split(",")[0])
+		#### read the original mpileup until reaching the corresponding line
+		while line_counter!=global_index:
 			line_counter+=1
+			original_line = original_mpileup.readline()
+		outf.write(original_line)
 	outf.close()
-
-
-
